bb41265d98aec03c29579e16bae536e11ce815d4
adolphenayituriki <www.nayituriki.com@gmail.com>
2026-09-24 12:20:05 +0200

    Prefer SITE_URL over RENDER_EXTERNAL_URL for email profile links

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -12,7 +12,7 @@ const escapeHtml = (value) =>
     .replace(/'/g, "&#39;");
 
 const siteUrl = () =>
-  (process.env.RENDER_EXTERNAL_URL || process.env.SITE_URL || "http://localhost:5000").replace(/\/+$/, "");
+  (process.env.SITE_URL || process.env.RENDER_EXTERNAL_URL || "http://localhost:5000").replace(/\/+$/, "");
 
 const getLogoUrl = () => process.env.BREVO_LOGO_URL || `${siteUrl()}/Logo.png`;
 
