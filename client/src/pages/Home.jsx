fc0f602cfab94bb7816cb485d7d120cf2276e7f1
adolphenayituriki <www.nayituriki.com@gmail.com>
2026-09-25 18:32:09 +0200

    Move ELITEFRAMSTUDIO(135).jpg upward with objectPosition: center -15%

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const featuredImages = [
   "/Featured Images/ELITEFRAMSTUDIO(73).jpg",
   "/Featured Images/ELITEFRAMSTUDIO(133) (1).jpg",
   "/Featured Images/ELITEFRAMSTUDIO(134).jpg",
-  { src: "/Featured Images/ELITEFRAMSTUDIO(135).jpg", objectPosition: "center top" },
+  { src: "/Featured Images/ELITEFRAMSTUDIO(135).jpg", objectPosition: "center -15%" },
   { src: "/Featured Images/WhatsApp Image 2026-09-25 at 16.41.13.jpeg", objectPosition: "center bottom" },
   "/Featured Images/WhatsApp Image 2026-09-25 at 16.48.45.jpeg",
   "/Featured Images/WhatsApp Image 2026-09-b25 at 16.41.13.jpeg",
